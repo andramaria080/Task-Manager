3fe329444b4e40a43ff1eb3e9aba21052fbea714
Andra <andramaria080@gmail.com>
2023-08-31 01:41:48 +0300

    Conditional Rendering and Styling tasks

--- src/App.jsx
+++ src/App.jsx
@@ -3,52 +3,50 @@ import "./App.css";
 import TaskViewer from "./components/task-viewer/TaskViewer";
 
 function App() {
-  // const data = [
-  //   {
-  //     id: "T-1",
-  //     status: "Todo",
-  //     name: "Create a Design System for Enum Workspace",
-  //     dueDate: new Date(2022, 3, 21),
-  //   },
-  //   {
-  //     id: "T-2",
-  //     status: "In progress",
-  //     name: "Create a Design System for Enum Workspace",
-  //     dueDate: new Date(2022, 4, 11),
-  //   },
-  //   {
-  //     id: "T-3",
-  //     status: "Complete",
-  //     name: "Create a Design System for Enum Workspace",
-  //     dueDate: new Date(2022, 5, 22),
-  //   },
-  //   {
-  //     id: "T-4",
-  //     status: "Complete",
-  //     name: "Create a Design System for Enum Workspace",
-  //     dueDate: new Date(2022, 5, 24),
-  //   },
-  //   {
-  //     id: "T-5",
-  //     status: "In progress",
-  //     name: "Create a Design System for Enum Workspace",
-  //     dueDate: new Date(2022, 5, 24),
-  //   },
-  //   {
-  //     id: "T-6",
-  //     status: "Todo",
-  //     name: "Create a Design System for Enum Workspace",
-  //     dueDate: new Date(2022, 5, 24),
-  //   },
-  //   {
-  //     id: "T-7",
-  //     status: "Todo",
-  //     name: "Create a Design System for Enum Workspace",
-  //     dueDate: new Date(2022, 5, 24),
-  //   },
-  // ];
-
-  const data = [];
+  const data = [
+    {
+      id: "T-1",
+      status: "Todo",
+      name: "Create a Design System for Enum Workspace",
+      dueDate: new Date(2022, 3, 21),
+    },
+    {
+      id: "T-2",
+      status: "In progress",
+      name: "Create a Design System for Enum Workspace",
+      dueDate: new Date(2022, 4, 11),
+    },
+    {
+      id: "T-3",
+      status: "Complete",
+      name: "Create a Design System for Enum Workspace",
+      dueDate: new Date(2022, 5, 22),
+    },
+    {
+      id: "T-4",
+      status: "Complete",
+      name: "Create a Design System for Enum Workspace",
+      dueDate: new Date(2022, 5, 24),
+    },
+    {
+      id: "T-5",
+      status: "In progress",
+      name: "Create a Design System for Enum Workspace",
+      dueDate: new Date(2022, 5, 24),
+    },
+    {
+      id: "T-6",
+      status: "To do",
+      name: "Create a Design System for Enum Workspace",
+      dueDate: new Date(2022, 5, 24),
+    },
+    {
+      id: "T-7",
+      status: "Panding",
+      name: "Create a Design System for Enum Workspace",
+      dueDate: new Date(2022, 5, 24),
+    },
+  ];
 
   const [taskList, setTaskList] = useState(data);
 
--- src/components/badge/Badge.jsx
+++ src/components/badge/Badge.jsx
@@ -1,8 +1,23 @@
 import "./Badge.css";
+import React from "react";
 
 function Badge(props) {
+  const getBadgeStyle = () => {
+    switch (props.status) {
+      case "ToDo":
+        return "grey";
+      case "In progress":
+        return "blue";
+      case "Complete":
+        return "green";
+      case "Panding":
+        return "orange";
+      default:
+        return "grey";
+    }
+  };
   return (
-    <div className="badge">
+    <div className={`badge ${getBadgeStyle()}`}>
       <p>{props.status}</p>
     </div>
   );
